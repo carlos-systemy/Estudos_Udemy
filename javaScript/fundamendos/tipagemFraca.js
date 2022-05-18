2da2eb1a85675726343cc97547b51cf1c33670b0
carlos-systemy <carlossilvafilho9@gmail.com>
2022-05-18 11:42:54 -0300

    tipagem fraca+ observações

---
+++ javaScript/fundamendos/tipagemFraca.js
@@ -0,0 +1,11 @@
+ let qualquer = 'legal';
+ console.log(qualquer);
+ console.log(typeof qualquer);
+
+ qualquer = 3.1516;
+ console.log(qualquer);
+ console.log(typeof qualquer);
+ //Evitar nome genéricos e siglas
+ //ex : let valor="";
+ // ou let numero = 1;
+ //ou let pqp = false;

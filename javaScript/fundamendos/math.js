a9e45994c10921df7acc1ae4b22c9d86cc8d25bd
carlos-systemy <carlossilvafilho9@gmail.com>
2022-05-18 13:28:39 -0300

    Observações

---
+++ javaScript/fundamendos/math.js
@@ -0,0 +1,6 @@
+const raio = 5.6;
+const area = Math.PI * Math.pow(raio, 2);//o atributo PI tem como  valor 3.14 e o atributo pow tem como função calcular potencia
+
+
+console.log(area);
+console.log(typeof Math);

97b2a3174a2a8a7d08db68224c851601debe70d9
carlos-systemy <carlossilvafilho9@gmail.com>
2022-05-19 10:41:08 -0300

    Exemplos de Funcoes

--- javaScript/fundamendos/Funcao1.js
+++ javaScript/fundamendos/Funcao1.js
@@ -1,7 +1,7 @@
 // Função sem retorno
 function imprimirSoma(a, b){
     console.log(a + b);
-}
+};
 
 imprimirSoma(2, 3); // Mostra a soma entre 2 e 3 = 5
 imprimirSoma(2); // Retorna not a Number(NaN) pois b é undefined, NÃO DA ERRO
@@ -11,7 +11,7 @@ imprimirSoma(); // NaN pois a e b são undefined, NÃO DA ERRO
 // Função com retorno
 function soma(a, b = 1){
     return a + b;
-}
+};
 
 console.log(soma(2, 3)); 
 console.log(soma(2)); // b tem um valor padrão definido na função, logo, se não for passado, ele utiliza essa valor
--- javaScript/fundamendos/Funcao2.js
+++ javaScript/fundamendos/Funcao2.js
@@ -1,13 +1,13 @@
 // Armazenando uma função em uma variável
 const imprimirSoma = function (a, b){ // Função anônima (sem nome)
     console.log(a + b);
-}
+};
 imprimirSoma(2, 3); // Chamando a função
 
 // Armazenando uma função arrow em uma variável
 const soma = (a, b) => {
     return a + b;
-}
+};
 console.log(soma(2, 3));
 
 // Retorno implícito
